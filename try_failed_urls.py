b8b7977798114bfdad7f7505e2b501e02ff0e1b9
Auss <a.abbood@live.de>
2018-12-16 19:21:37 +0100

    Rewrite failed_urls

--- try_failed_urls.py
+++ try_failed_urls.py
@@ -9,6 +9,7 @@ except FileNotFoundError:
     print('Your download meta data is missing. Consider download failed URLs by hand.')
 
 failed_index = []
+failed_urls = []
 try:
     with open('failed_urls.txt') as handler:
         for line in handler:
@@ -17,8 +18,13 @@ try:
                 index = re.search(r'\[(\d+)\]', to_parse)[1]
                 failed_index.append(int(index))
             except TypeError:
+                failed_urls.append(to_parse)
                 continue
     os.remove('failed_urls.txt')
+    with open('failed_urls.txt', 'a') as file:
+        for failed_url in failed_urls:
+            file.write(failed_url + '\n')
+
 except FileNotFoundError:
     print('There are no failed URLs.')
 
