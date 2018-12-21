e3bbebac688b0d60d4ff56b8202f51ee32373d3f
Auss <a.abbood@live.de>
2018-12-21 10:24:18 +0100

    Working script to retry failed URLs

---
+++ try_failed.py
@@ -0,0 +1,34 @@
+import os
+import re
+import pickle
+from tqdm import tqdm
+from archive import download
+
+
+def get_index_num(listdir):
+    # Get the missing indices to know which file to retry
+
+    index = []
+    for i in range(len(listdir)):
+        try:
+            index.append(re.match(r'<(\d+)_?\d*>', listdir[i])[1])
+        except (TypeError, IndexError):
+            print(listdir[i])
+    index = [int(i[:4]) for i in index]
+    index = sorted(list(set(index)))
+    failed_indices = set(list(range(max(index)))) - set(index)
+    return failed_indices
+
+
+if __name__ == '__main__':
+    images = os.listdir('tumblr_images')
+    videos = os.listdir('tumblr_videos')
+    images.extend(videos)
+    failed = get_index_num(images)
+
+    posts_pickle = pickle.load(open('posts.p', 'rb'))
+
+    for index in tqdm(failed):
+        download([posts_pickle[index]], start=0, to_index=index, disable=True)
+
+
